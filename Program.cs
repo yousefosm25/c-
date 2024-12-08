5d72740851c4df7cf474f07f07aed374b61a3df2
yousefosm25 <yousefmouse1@gmail.com>
2024-12-09 00:34:39 +0200

    my first project

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace cansoleapp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is a C# console application.");
+            Console.WriteLine("Wellcome to my calculator "); 
+            Console.WriteLine("press Enter to continue...");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("Enter the number1 ");
+            double y = 0;
+            double.TryParse(Console.ReadLine(), out y);
+            Console.Clear();
+            Console.WriteLine("Enter the number2 ");
+            double z = 0;
+            double.TryParse(Console.ReadLine(), out z);
+            Console.Clear();
+            Console.WriteLine("Enter the number for the calculation \n if you want to + enter number 1 \n if you want to - enter number 2 \n if you want to / enter number 3");
+            int x = 0;
+            int.TryParse(Console.ReadLine(), out x);
+            Console.Clear();
+            switch (x)
+            {
+                case 1:
+                Console.WriteLine(y + z);
+                break;
+                case 2:
+                Console.WriteLine(y - z);
+                break;
+                case 3:
+                Console.WriteLine(y / z);
+                break;
+                default:
+                Console.WriteLine("Invalid input. Please enter 1, 2, or 3.");
+                break;
+
+            }
+
+            
+        }
+    }
+}
